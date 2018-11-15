178b1635c9d2a6b08caeaf0418bd3bc0b4893d02
johson chinonso <johnsonchinonso19@gmail.com>
2018-11-15 10:06:01 +0100

    create class that extends shipmentsearchindex

---
+++ src/shipmentUpdateListener.ts
@@ -0,0 +1,13 @@
+import ShipmentSearchIndex from './shipmentSearchIndex';
+
+interface ShipmentUpdateListenerInterface {
+  receiveUpdate(id: string, shipmentData: any) 
+}
+
+class ShipmentUpdateListener extends ShipmentSearchIndex implements ShipmentUpdateListenerInterface {
+  async receiveUpdate(id: string, shipmentData: any) {
+    await this.updateShipment(id, shipmentData)
+  }
+}
+
+
